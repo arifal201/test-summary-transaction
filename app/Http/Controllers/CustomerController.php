73c0708d7ce3d540b7d6ee04f4cbb94f84ecc545
arifal201 <arifalhidayat10@gmail.com>
2023-05-31 23:00:01 +0700

    create crud product and customer

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+        return response()->json($customers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = Customer::create($request->all());
+        return response()->json($customer);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+        return response()->json($customer);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $customer = Customer::find($id);
+        $customer->update($request->all());
+        return response()->json($customer);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customer = Customer::find($id);
+        return response()->json('success delete');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', 'ProductController@index');
+Route::get('products/{id}', 'ProductController@show');
+Route::post('products', 'ProductController@store');
+Route::put('products/{id}', 'ProductController@update');
+Route::delete('products/{id}', 'ProductController@destroy');
+
+Route::get('customers', 'CustomerController@index');
+Route::get('customers/{id}', 'CustomerController@show');
+Route::post('customers', 'CustomerController@store');
+Route::put('customers/{id}', 'CustomerController@update');
+Route::delete('customers/{id}', 'CustomerController@destroy');
