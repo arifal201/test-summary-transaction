3bda40bbf7603b567ed607d56b4e5be50b41cf1b
arifal201 <arifalhidayat10@gmail.com>
2023-06-03 16:08:48 +0700

    enable cors

--- routes/api.php
+++ routes/api.php
@@ -13,26 +13,28 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(['cors'])->group( function(){
+    Route::get('products', 'ProductController@index');
+    Route::get('products/{id}', 'ProductController@show');
+    Route::post('products', 'ProductController@store');
+    Route::put('products/{id}', 'ProductController@update');
+    Route::delete('products/{id}', 'ProductController@destroy');
+    
+    Route::get('customers', 'CustomerController@index');
+    Route::get('customers/{id}', 'CustomerController@show');
+    Route::post('customers', 'CustomerController@store');
+    Route::put('customers/{id}', 'CustomerController@update');
+    Route::delete('customers/{id}', 'CustomerController@destroy');
+    
+    Route::get('transactions', 'TransactionController@index');
+    Route::get('transactions/{id}', 'TransactionController@show');
+    Route::post('transactions', 'TransactionController@store');
+    Route::put('transactions/{id}', 'TransactionController@update');
+    Route::delete('transactions/{id}', 'TransactionController@destroy');
+    
+    Route::get('summary', 'TransactionController@summary');
 });
-
-Route::get('products', 'ProductController@index');
-Route::get('products/{id}', 'ProductController@show');
-Route::post('products', 'ProductController@store');
-Route::put('products/{id}', 'ProductController@update');
-Route::delete('products/{id}', 'ProductController@destroy');
-
-Route::get('customers', 'CustomerController@index');
-Route::get('customers/{id}', 'CustomerController@show');
-Route::post('customers', 'CustomerController@store');
-Route::put('customers/{id}', 'CustomerController@update');
-Route::delete('customers/{id}', 'CustomerController@destroy');
-
-Route::get('transactions', 'TransactionController@index');
-Route::get('transactions/{id}', 'TransactionController@show');
-Route::post('transactions', 'TransactionController@store');
-Route::put('transactions/{id}', 'TransactionController@update');
-Route::delete('transactions/{id}', 'TransactionController@destroy');
-
-Route::get('summary', 'TransactionController@summary');
